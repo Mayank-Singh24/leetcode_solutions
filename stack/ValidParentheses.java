0cabfdfa6fa78fb999620257890c97059656a163
Mayank-Singh24 <mayankis2468@gmail.com>
2026-05-06 17:53:12 +0530

    Add solutions for stack

---
+++ stack/ValidParentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if(s.length() % 2 != 0){
+            return false;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(s.charAt(i) == ')' && stack.peek() == '('){
+                    stack.pop();
+                }
+                else if(s.charAt(i) == ']' && stack.peek() == '['){
+                    stack.pop();
+                }
+                else if(s.charAt(i) == '}' && stack.peek() == '{'){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
